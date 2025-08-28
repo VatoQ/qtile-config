df5a754567c4c14fae295243e9b8807259d998b5
Jonathan Enns <jonathanenns97@web.de>
2025-08-28 10:48:32 +0200

    rename const names

--- my_utils.py
+++ my_utils.py
@@ -23,23 +23,35 @@ TIMER_MINUTES = 30
 #### COMMON PROGRAMS AND BINDINGS ####
 ######################################
 
-mod = "mod4"
-terminal = "kitty"  # guess_terminal()
-power_menu = "~/.config/qtile/scripts/powermenu"
+MOD = "mod4"
+TERMINAL = "kitty"
+POWER_MENU = "~/.config/qtile/scripts/powermenu"
+SHIFT = "shift"
+CONTROL = "control"
+SPACE = "space"
+
+
 
 #######################
-#### COLOR_SECTION ####
+#### COLOR SECTION ####
 #######################
+# not really fitting, consider adding later: #EEFF28
+# not really fitting, consider adding later: #FF28EE
+# not really fitting, consider adding later: #EE28FF
+# not really fitting, consider adding later: #28FFEE
+# not really fitting, consider adding later: #28EEFF
+YELLOW =        "#FFEE28"
+ORANGE =        "#FFAA28"
+RED =           "#FF2828"
+MAGENTA =       "#FF28AA"
+PURPLE =        "#AA28FF"
+BLUE =          "#2828FF"
+CYAN =          "#28AAFF"
+LIGHT_GREEN =   "#28FFAA"
+GREEN =         "#28FF28"
+LIME =          "#AAFF28"
+GRAY =          "#A2A29A"
 
-YELLOW      = "#FFEE28"
-ORANGE      = "#FFAA28"
-MAGENTA     = "#FF28AA"
-RED         = "#F02850"
-LIME        = "#AAFF28"
-LIGHT_GREEN = "#28FFAA"
-CYAN        = "#28AAFF"
-BLUE        = "#2828FF"
-GRAY        = "#ABABAB"
 
 
 def dim_color(color:str, quotient:float) -> str:
@@ -53,14 +65,14 @@ def dim_color(color:str, quotient:float) -> str:
     r = int(color[1:3], 16)
     g = int(color[3:5], 16)
     b = int(color[5:7], 16)
-    color:list[int] = [r, g, b]
+    color_int:list[int] = [r, g, b]
     color_str:list[str] = []
 
 
-    for i in range(len(color)):
-        color[i] = int(color[i] / quotient)
-
-        color_str.append(f"{color[i]:02x}")
+    for ci in color_int:
+        ci = int(ci / quotient)
+        ci = min(255, ci)
+        color_str.append(f"{ci:02x}")
 
     return "#" + "".join(color_str)
 
@@ -77,13 +89,9 @@ POINT2 = (0, 1)
 ########################
 
 BAR_NORMAL = 0
-
 BAR_CHOICE_LEFT = 1
-
 BAR_CHOICE_RIGHT = 2
-
 BAR_CHOICE_LEFT_SLASH = 3
-
 BAR_CHOICE_RIGHT_SLASH = 4
 
 def get_decoration_group(choice: int):
@@ -101,7 +109,6 @@ def get_decoration_group(choice: int):
     elif choice == BAR_CHOICE_LEFT:
         result["decorations"] = [
             RectDecoration(
-                # colour="#004040",
                 use_widget_background=True,
                 padding_y=5,
                 filled=True,
@@ -111,12 +118,10 @@ def get_decoration_group(choice: int):
         ]
 
         result["padding"] = 15
-        # result["height"] = 30
 
     elif choice == BAR_CHOICE_RIGHT:
         result["decorations"] = [
             RectDecoration(
-                # colour="#004040",
                 use_widget_background=True,
                 padding_y=5,
                 filled=True,
