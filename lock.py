0f88c443d1ccfbdca34660665824b637cbafb7cf
Jonathan Enns <jonathanenns97@web.de>
2025-08-28 21:11:59 +0200

    make compliant with pylint

--- lock.py
+++ lock.py
@@ -1,31 +1,17 @@
 #!/bin/python
 
-import sys
-import os
 
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from my_utils import (
-        BLUE,
-        CYAN,
-        GRAY,
-        GREEN,
-        RED,
-        ORANGE,
-        PURPLE,
-        YELLOW,
-        FOCUS_COLOR,
-        dim_color
-)
 import subprocess
+from my_utils import *
 
-BLANK = dim_color(GRAY, 5) + "00"
-CLEAR = dim_color(GRAY, 0.5) + "22"
-DEFAULT = dim_color(FOCUS_COLOR, 1.0) + "CC"
-TEXT = dim_color(FOCUS_COLOR, 1.3) + "EE"
-WRONG = dim_color(RED, 1.0) + "BB"
-NOT_WRONG = dim_color(GREEN, 1.0) + "BB"
-VERIFYING = dim_color(YELLOW, 1.5) + "BB"
+
+BLANK       = dim_color_alpha(GRAY, DARK2, "00")
+CLEAR       = dim_color_alpha(GRAY, LIGHT, "22")
+DEFAULT     = dim_color_alpha(FOCUS_COLOR, NEUTRAL, "CC")
+TEXT        = dim_color_alpha(FOCUS_COLOR, DIMMED, "EE")
+WRONG       = dim_color_alpha(RED, NEUTRAL, "BB")
+NOT_WRONG   = dim_color_alpha(GREEN, NEUTRAL, "BB")
+VERIFYING   = dim_color_alpha(YELLOW, DIMMED2, "BB")
 
 command = [
     "i3lock",
@@ -57,4 +43,8 @@ command = [
     "--keylayout", "1"
 ]
 
-subprocess.run(command)
+
+
+
+
+_ = subprocess.run(command, check=False)
