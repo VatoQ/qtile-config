381e6d4fac430b28def5168ce8c665f25e0af5ac
Jonathan Enns <jonathanenns97@web.de>
2025-08-29 17:49:59 +0200

    refactor to modularize

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+"""
 # Copyright (c) 2010 Aldo Cortesi
 # Copyright (c) 2010, 2014 dequis
 # Copyright (c) 2012 Randall Ma
@@ -23,160 +24,35 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""
 
-from ast import mod
-from my_utils import *
-from tkinter import Y
+import os
+import subprocess
+import copy
 from libqtile import bar, layout, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from qtile_extras.widget.decorations import GradientDecoration, PowerLineDecoration, RectDecoration
 from libqtile import hook
 from qtile_extras import widget as extrawidgets
-from clock_widget import extended_clock, ExtendedClock
+#from clock_widget import extended_clock, ExtendedClock
 from libqtile.backend.wayland.inputs import InputConfig
 from qtile_extras.layout.decorations.borders import GradientFrame
-import os
-import subprocess
-from qtile_graphs import show_graphs
-import random_wallpaper
+#import random_wallpaper
+import bar_layout
+from keybindings import get_keys
+from my_utils import (
+        FOCUS_COLOR,
+        MOD,
+        SHIFT,
+        init_layout_theme,
+        MAX_BORDER_WIDTH,
+        MIN_BORDER_WIDTH,
+        )
 
-launcher = ""
-file_manager = ""
 
-if qtile.core.name == "x11":
-    term = "urxvt"
-    launcher = "rofi -show drun"
-    file_manager = "pcmanfm"
-elif qtile.core.name == "wayland":
-    term = "foot"
-    launcher = "wofi --show drun"
-    file_manager = "dolphin"
+keys = get_keys()
 
-keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([MOD, SHIFT], "g", lazy.function(show_graphs)),
-    Key([MOD], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([MOD], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([MOD], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([MOD], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([MOD], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key(
-        [MOD, SHIFT], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
-    Key(
-        [MOD, SHIFT],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key([MOD, SHIFT], "j",
-        lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([MOD, SHIFT], "k",
-        lazy.layout.shuffle_up(),
-        desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([MOD, "control"], "h",
-        lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key(
-        [MOD, "control"], "l",
-        lazy.layout.grow_right(),
-        desc="Grow window to the right"
-    ),
-    Key([MOD, "control"], "j",
-        lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([MOD, "control"], "k",
-        lazy.layout.grow_up(),
-        desc="Grow window up"),
-    Key([MOD], "n",
-        lazy.layout.normalize(),
-        desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [MOD, SHIFT],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key(
-        [MOD, SHIFT],
-        "q",
-        lazy.spawn(os.path.expanduser(POWER_MENU)),
-        desc="Spawn power menu",
-        ),
-    Key([MOD], "q",
-        lazy.spawn(TERMINAL),
-        desc="Launch terminal"),
-    # Key(["control", "alt"], "t", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([MOD], "Tab",
-        lazy.next_layout(),
-        desc="Toggle between layouts"),
-    Key([MOD], "c",
-        lazy.window.kill(),
-        desc="Kill focused window"),
-    Key(
-        [MOD],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
-    Key(
-        [MOD],
-        "t",
-        lazy.window.toggle_floating(),
-        desc="Toggle floating on the focused window",
-    ),
-    Key([MOD, "control"], "r",
-        lazy.reload_config(),
-        desc="Reload the config"),
-    Key([MOD, "control"], "q",
-        lazy.shutdown(),
-        desc="Shutdown Qtile"),
-    Key([MOD], "r",
-        lazy.spawn(launcher),
-        desc="Spawn a command using a prompt widget"),
-    Key([MOD], "e",
-        lazy.spawn(file_manager),
-        desc="Spawn file manager"),
-    # Switching workspaces
-    Key([MOD], "right",
-        lazy.screen.next_group(skip_empty=True)),
-    Key([MOD], "left",
-        lazy.screen.prev_group(skip_empty=True)),
-    Key([MOD, SHIFT], "right",
-        lazy.screen.next_group(skip_empty=False)),
-    # Key([mod], "up", lazy.layout.focus_next()),
-    # Key([mod], "down", lazy.layout.focus_previous()),
-    Key([], "XF86AudioRaiseVolume",
-        lazy.widget["volume"].increase_vol(),
-        desc="Increase Volume"),
-    Key([], "XF86AudioLowerVolume",
-        lazy.widget["volume"].decrease_vol(),
-        desc="Decrease Volume"),
-    Key([], "XF86AudioMute",
-        lazy.widget["volume"].mute(),
-        desc="Toggle Mute"),
-    Key([], "XF86MonBrightnessUp",
-        lazy.widget["brightness"].brightness_up(),
-        desc="Brightness Up"),
-    Key([], "XF86MonBrightnessDown",
-        lazy.widget["brightness"].brightness_down(),
-        desc="Brightness Down"),
-    Key([CONTROL, SHIFT], "l",
-        lazy.spawn(os.path.expanduser(LOCK_SCREEN)))
-]
 
 # Add key bindings to switch VTs in Wayland.
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
@@ -190,7 +66,6 @@ for vt in range(1, 8):
             desc=f"Switch to VT{vt}",
         )
     )
-
 
 groups = [Group(i) for i in "123456789"]
 
@@ -255,118 +130,9 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 
-decoration_group = get_decoration_group(BAR_NORMAL)
-decorations_left = get_decoration_group(BAR_CHOICE_LEFT)
-decorations_right = get_decoration_group(BAR_CHOICE_RIGHT)
-
-# some widgets look better with slightly other settings:
-workspace_decoration = decorations_left.copy()
-workspace_decoration["padding"] = 6
-workspace_decoration["decorations"][0] = RectDecoration(
-    use_widget_background=True,
-    padding_y=20,
-    filled=True,
-    radius=9,
-)
-
-
-wifi_decoration =  decorations_right.copy()
-wifi_decoration["padding"] = 8
-
-
 screens = [
     Screen(
-        top=bar.Bar(
-            [
-                extrawidgets.CurrentLayoutIcon(
-                    background=dim_color(BLUE, DARKER), 
-                    **decorations_left
-                ),
-                extrawidgets.GroupBox2(
-                    fontsize=20,
-                    highlight_method="block",
-                    background=dim_color(GRAY, DARK),
-                    hide_unused=True,
-                    rules=GROUPBOX_RULES,
-                    **workspace_decoration,
-                ),
-                extrawidgets.WindowName(
-                    background=dim_color(GRAY, MEDIUM2),
-                    **decorations_right
-                ),
-                # extrawidgets.StatusNotifier(background="#001020", **decorations_right),
-                extrawidgets.WiFiIcon(
-                    background=dim_color(BLUE, DARK2),
-                    interface="wlp2s0",
-                    wifi_arc=75,
-                    **wifi_decoration,
-                    ),
-                extrawidgets.Bluetooth(
-                    background=dim_color(BLUE, DARK2),
-                    default_text="󰂯 {connected_devices}",
-                    **decorations_right
-                    ),
-                extrawidgets.Systray(
-                    background=dim_color(BLUE, DARK2),
-                    **decorations_right
-                    ),
-                extrawidgets.Clock(
-                    background=dim_color(BLUE, DARK),
-                    fontsize=11,
-                    format="%a %d.%m.%y\n %H:%M:%S %p",
-                    **decorations_right,
-                ),
-                extrawidgets.Battery(
-                    background=dim_color(BLUE, DARKER),
-                    format="{char} {percent:2.0%}",
-                    empty_char="󰁺",
-                    discharge_char="󰁿",
-                    charge_char="󰂄",
-                    full_char="󰁹",
-                    low_background=RED,
-                    low_percentage=0.2,
-                    charge_controller=lambda: (0, 95),
-                    update_interval= 4,
-                    **decorations_right,
-                ),
-                extrawidgets.BrightnessControl(
-                    name="brightness",
-                    background=dim_color(CYAN, MEDIUM2),
-                    bar_colour=dim_color(CYAN, DARKER2),
-                    bar_height=25,
-                    min_brightness=35,
-                    device="/sys/class/backlight/amdgpu_bl1",
-                    format="{percent:2.0%}",
-                    step=5,
-                    **decorations_right
-                ),
-                extrawidgets.PulseVolume(
-                    name="volume",
-                    volume_app = "pavucontrol",
-                    emoji_list = [
-                        "",
-                        "",
-                        "",
-                        "",
-                        ],
-                    background = dim_color(CYAN, DIMMED2), 
-                    emoji=True, 
-                    **decorations_right
-                ),
-                extrawidgets.ScriptExit(
-                    background=dim_color(RED, NEUTRAL),
-                    default_text="󰅙",  # "",
-                    countdown_format="{}",
-                    countdown_start=5,
-                    **decorations_right,
-                ),
-                # extended_clock,
-            ],
-            35,
-            # radius=10,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
+        top=bar_layout.bar_layout(),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
@@ -380,7 +146,6 @@ screens = [
 # Drag floating layouts.
 mouse = [
     Drag(
-        # [mod],
         [MOD],
         "Button1",
         lazy.window.set_position_floating(),
@@ -413,8 +178,8 @@ floating_layout = layout.Floating(
         Match(title="Figure 1"),
         Match(wm_class="powermenu"),
     ],
-    max_border_width=4,
-    border_width=4,
+    max_border_width=MAX_BORDER_WIDTH,
+    border_width=MIN_BORDER_WIDTH,
     border_focus=FOCUS_COLOR,
 )
 auto_fullscreen = True
@@ -432,7 +197,7 @@ wl_input_rules = {
 }
 # xcursor theme (string or None) and size (integer) for Wayland backend
 wl_xcursor_theme = None
-wl_xcursor_size = 24
+wl_xcursor_size = 25
 
 
 sticky_windows = []
