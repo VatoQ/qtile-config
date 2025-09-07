0b24e3b2c4916ef2feee2fc0fffd42659cba8d76
Jonathan Enns <jonathanenns97@web.de>
2025-09-07 22:30:00 +0200

    add task list to bar

--- bar_layout.py
+++ bar_layout.py
@@ -1,6 +1,13 @@
 """
 Contains `bar_layout()`, which returns a Bar object.
 """
+# pyright: reportUnknownVariableType=false
+# pyright: reportAny=false
+# pyright: reportUnknownParameterType=false
+# pyright: reportUnknownArgumentType=false
+# pyright: reportMissingParameterType=false
+# pyright: reportUntypedFunctionDecorator=false
+# pyright: reportUnknownMemberType=false
 
 import copy
 from qtile_extras import widget as extrawidgets
@@ -8,9 +15,12 @@ from qtile_extras.widget.decorations import RectDecoration
 from libqtile import bar, lazy
 from my_utils import (
     BLUE,
+    DIMMED,
+    FOCUS_COLOR,
     GRAY,
     CYAN,
     RED,
+    SECONDARY_COLOR,
     dim_color,
     DARK,
     DARK2,
@@ -34,13 +44,13 @@ def bar_layout() -> bar.Bar:
 
     :return: a Bar object that contains some widgets with decorations.
     """
-    decoration_group = get_decoration_group(BAR_NORMAL)
+    # decoration_group = get_decoration_group(BAR_NORMAL)
     decorations_left = get_decoration_group(BAR_CHOICE_LEFT)
     decorations_right = get_decoration_group(BAR_CHOICE_RIGHT)
 
     # some widgets look better with slightly other settings:
     workspace_decoration = decorations_left.copy()
-    workspace_decoration["padding"] = 6
+    workspace_decoration["padding"] = 4
     workspace_decoration["decorations"][0] = RectDecoration(
         use_widget_background=True,
         padding_y=20,
@@ -54,57 +64,108 @@ def bar_layout() -> bar.Bar:
 
     most_right_widget_deccoration = copy.deepcopy(decorations_right)
     most_right_widget_deccoration["decorations"] = [
-            RectDecoration(
-                use_widget_background=True,
-                padding_y=-16,
-                filled=True,
-                radius=9,
-                )
-            ]
+        RectDecoration(
+            use_widget_background=True,
+            padding_y=-16,
+            filled=True,
+            radius=9,
+        )
+    ]
+
+    tasklist_decorations = copy.deepcopy(decorations_right)
+    tasklist_decorations["padding"] = 6
+
+
+    dark_blue = dim_color(BLUE, DARK2)
+    medium_blue = dim_color(BLUE, DARKER2)
+    light_blue = dim_color(BLUE, DARKER)
+
 
     return bar.Bar(
         [
             extrawidgets.CurrentLayoutIcon(
-                background=dim_color(BLUE, DARKER), 
+                background=dim_color(SECONDARY_COLOR, DARKER2), 
+                use_mask=True,
+                margin_y=3,
+                foreground=[
+                    dim_color(FOCUS_COLOR, NEUTRAL),
+                    dim_color(SECONDARY_COLOR, DIMMED),
+                    dim_color(FOCUS_COLOR, NEUTRAL)],
                 **decorations_left
             ),
             extrawidgets.GroupBox2(
-                fontsize=20,
+                font="FiraCode Nerd Font",
+                fontsize=19,
                 highlight_method="block",
                 background=dim_color(GRAY, DARK),
                 hide_unused=True,
                 rules=GROUPBOX_RULES,
                 **workspace_decoration,
             ),
-            extrawidgets.WindowName(
+            # extrawidgets.WindowCount(
+            #     background=dim_color(GRAY, MEDIUM2),
+            #     font_shadow=dim_color(GRAY, DARK),
+            #     text_format="  : {num}",
+            #     **wifi_decoration
+            #     ),
+            # extrawidgets.WindowName(
+            #     background=dim_color(GRAY, MEDIUM2),
+            #     fontshadow=dim_color(GRAY, DARK),
+            #     width=400,
+            #     scroll=True,
+            #     scroll_fixed_width=True,
+            #     **decorations_right
+            # ),
+            extrawidgets.TaskList(
                 background=dim_color(GRAY, MEDIUM2),
-                **decorations_right
-            ),
+                border=dim_color(FOCUS_COLOR, NEUTRAL),
+                unfocused_border=dim_color(FOCUS_COLOR, DIMMED2),
+                fontshadow=dim_color(GRAY, DARK),
+                scroll=True,
+                highlight_method="block",
+                max_title_width=200,
+                txt_floating="  ",
+                txt_maximized="  ",
+                txt_minimized="  ",
+                **tasklist_decorations
+                ),
+            # extrawidgets.Spacer(
+            #     background=dim_color(GRAY, MEDIUM2),
+            #     hide_crash=True,
+            #     **decorations_right,
+            #     ),
             extrawidgets.WiFiIcon(
-                background=dim_color(BLUE, DARK2),
+                background=dark_blue,
                 interface="wlp2s0",
                 wifi_arc=75,
                 **wifi_decoration,
                 ),
             extrawidgets.Bluetooth(
-                background=dim_color(BLUE, DARK2),
+                background=dark_blue,
                 fontsize=25,
                 default_text="󰂯 {connected_devices}",
-                **decorations_right
+                **wifi_decoration,
+                ),
+            extrawidgets.CheckUpdates(
+                background=dark_blue,
+                distro="Arch",
+                no_update_string="✅",
+                display_format="󰚰: {updates}",
+                **wifi_decoration,
                 ),
             extrawidgets.Systray(
-                background=dim_color(BLUE, DARK2),
+                background=dark_blue,
                 hide_crash=True, # Systray tends to crash on reload because of singleton rule
                 **decorations_right
                 ),
             extrawidgets.Clock(
-                background=dim_color(BLUE, DARKER2),
+                background=medium_blue,
                 #fontsize=11,
                 format="%a %d.%m.%y",
                 #**decorations_right,
             ),
             extrawidgets.AnalogueClock(
-                background=dim_color(BLUE, DARKER2),
+                background=medium_blue,
                 face_shape="circle",
                 face_border_colour=dim_color(CYAN, NEUTRAL),
                 second_colour=dim_color(CYAN, LIGHT),
@@ -119,7 +180,7 @@ def bar_layout() -> bar.Bar:
                 **decorations_right,
             ),
             extrawidgets.Battery(
-                background=dim_color(BLUE, DARKER),
+                background=light_blue,
                 format="{char} {percent:2.0%}",
                 empty_char="󰁺",
                 discharge_char="󰁿",
@@ -159,7 +220,7 @@ def bar_layout() -> bar.Bar:
             ),
             extrawidgets.Image(
                 background=dim_color(RED, DIMMED2),
-                colour=dim_color(RED, LIGHT / 1.8),
+                colour=dim_color(RED, LIGHT / 2.),
                 filename="~/Pictures/Icons/qtile/bar/logout.png",
                 mask=True,
                 margin_y=3,
