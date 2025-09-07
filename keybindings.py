0765c7738819c42a49e372996b0ae2831971aea5
Jonathan Enns <jonathanenns97@web.de>
2025-09-07 22:32:30 +0200

    keybindings now conditional on layout

--- keybindings.py
+++ keybindings.py
@@ -2,10 +2,19 @@
 Contains `get_keys()`, which returns the list of keybindings to
 be used by qtile.
 """
+# pyright: reportUnknownVariableType=false
+# pyright: reportAny=false
+# pyright: reportUnknownParameterType=false
+# pyright: reportUnknownArgumentType=false
+# pyright: reportMissingParameterType=false
+# pyright: reportUntypedFunctionDecorator=false
+# pyright: reportUnknownMemberType=false
 
 import os
+from datetime import datetime
 from libqtile.config import Key
 from libqtile.lazy import lazy
+from libqtile.utils import send_notification
 from my_utils import (
     MOD,
     SHIFT,
@@ -18,6 +27,65 @@ from my_utils import (
 from qtile_graphs import show_graphs
 from powermenu import show_power_menu
 
+def move_window(qtile, direction:str) -> None:
+    """
+    decide, which action to take when [MOD, SHIFT], [VIM KEY] is pressed, based on current layout.
+
+    :param qtile Qtile: Current Qtile instance, passed by lazy object.
+    :param direction: one of `(left, right, up, down)`
+    """
+    error_log:list[str] = []
+
+    def log_message(*args:str) -> None:
+        location = os.path.expanduser("~/.config/qtile/move_log.txt")
+        with open(location,mode="a", encoding="utf-8") as log_file:
+            current_time = datetime.now()
+            _ = log_file.write(current_time.strftime("%a %m.%d.%Y, %H:%M:%S") + "\n")
+            for message in args:
+                message = "\t" + message + "\n"
+                _ = log_file.write(message)
+            _ = log_file.write("\n")
+
+
+    if direction not in [
+            "left",
+            "right",
+            "up",
+            "down"
+            ]:
+        message = f"{direction} is not a valid direction"
+        _ = send_notification(
+                "qtile",
+                message
+                )
+        error_log.append(message)
+        log_message(*error_log)
+
+        return
+
+    layout = qtile.current_group.layout
+    layout_name = layout.name
+
+    if layout_name == "bsp":
+        method = getattr(layout, f"shuffle_{direction}", None)
+    elif layout_name == "treetab":
+        method = getattr(layout, f"move_{direction}", None)
+    else:
+        error_log.append(f"couldn't resolve layout {layout_name}")
+        log_message(*error_log)
+        return
+
+    if method:
+        method()
+        log_message(f"Executed {method.__name__} for layout {layout_name}")
+    else:
+        error_log.append("method is None")
+        log_message(*error_log)
+        _ = send_notification(
+                "qtile",
+                f"No method found for {direction} in {layout_name}"
+                )
+
 
 def get_keys() -> list[Key]:
     """
@@ -28,7 +96,8 @@ def get_keys() -> list[Key]:
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    keybindings = [ Key([MOD, SHIFT], "g", lazy.function(show_graphs)),
+    keybindings = [ 
+    Key([MOD, SHIFT], "g", lazy.function(show_graphs)),
     Key([MOD], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([MOD], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([MOD], "j", lazy.layout.down(), desc="Move focus down"),
@@ -38,19 +107,26 @@ def get_keys() -> list[Key]:
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key(
-        [MOD, SHIFT], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+        [MOD, SHIFT], 
+        "h", 
+        # lazy.layout.shuffle_left(),
+        lazy.function(lambda qtile: move_window(qtile, "left")),
+        desc="Move window to the left"
     ),
     Key(
         [MOD, SHIFT],
         "l",
-        lazy.layout.shuffle_right(),
+        # lazy.layout.shuffle_right(),
+        lazy.function(lambda qtile: move_window(qtile, "right")),
         desc="Move window to the right",
     ),
     Key([MOD, SHIFT], "j",
-        lazy.layout.shuffle_down(),
+        # lazy.layout.shuffle_down(),
+        lazy.function(lambda qtile: move_window(qtile, "down")),
         desc="Move window down"),
     Key([MOD, SHIFT], "k",
-        lazy.layout.shuffle_up(),
+        # lazy.layout.shuffle_up(),
+        lazy.function(lambda qtile: move_window(qtile, "up")),
         desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
